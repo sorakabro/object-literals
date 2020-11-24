ac772e917ccd22d79f48cc61946f7e31b036b182
Benjamin Oldén <berntsson90@hotmail.com>
2020-11-24 09:38:58 +0100

    syntax for objects

--- main.js
+++ main.js
@@ -20,4 +20,27 @@ let maria = {
     gender: "Female"
 }
 
-console.log(benjamin, maria);
+
+
+// Vi kan lägga till egenskaper på ett simpelt sätt på någon av objekten.
+
+benjamin.address = "Hamngatan 12321";
+benjamin.name = "Olle";
+
+// Vi kan även ändra på egenskaper i ett objekt. det finns två sätt att göra detta på. Första är via punktnotation.
+
+benjamin.age = 31;
+
+// Det andra sätter är på ett arrayliknande sätt.
+
+benjamin["age"] = 45;
+benjamin["gender"] = "Female";
+
+
+// Vill vi ta bort en egenskap, vilket man sällan gör. Istället brukar man sätta värdet till null eller 0
+
+
+delete benjamin.age;
+delete benjamin["age"];
+
+console.log(benjamin);
